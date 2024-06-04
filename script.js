8f98a2b695e33b35036e998ab0d6df8c18be26b8
Justin Park <jutpark@ucsc.edu>
2024-06-03 20:48:29 -0700

    rechanging trees to 1

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //the website has been iteratively designed using chatGPT to create a basic quiz system. From here I will be implementing the P5.js and data vis but the quiz functionally works. I'll need to add shuffling for questions once I have them. 
 let questions = [
     { question: "Where is this tree?", correctAnswer: 3, img: 'tree1.jpg' }, // College 9/10 button index
-    { question: "Where is this tree?", correctAnswer: 2, img: 'tree2.jpg' }, // BSOE button index
-    { question: "Where is this tree?", correctAnswer: 4, img: 'tree3.jpg' }, // Crown/Merrill button index
-    { question: "Where is this tree?", correctAnswer: 0, img: 'tree4.jpg' }, // Oakes/RCC button index
-    { question: "Where is this tree?", correctAnswer: 1, img: 'tree5.jpg' }, // Porter/Kresge button index
-    { question: "Where is this tree?", correctAnswer: 5, img: 'tree6.jpg' }, // Mchenry button index
-    { question: "Where is this tree?", correctAnswer: 6, img: 'tree7.jpg' }, // Cowell/Stevenson button index
-    { question: "Where is this tree?", correctAnswer: 3, img: 'tree8.jpg' }, // College 9/10 button index
-    { question: "Where is this tree?", correctAnswer: 4, img: 'tree9.jpg' }, // Crown/Merrill button index
-    { question: "Where is this tree?", correctAnswer: 2, img: 'tree10.jpg' } // BSOE button index
+    { question: "Where is this tree?", correctAnswer: 2, img: 'tree2.jpeg' }, // BSOE button index
+    { question: "Where is this tree?", correctAnswer: 4, img: 'tree1.jpg' }, // Crown/Merrill button index
+    { question: "Where is this tree?", correctAnswer: 0, img: 'tree1.jpg' }, // Oakes/RCC button index
+    { question: "Where is this tree?", correctAnswer: 1, img: 'tree1.jpg' }, // Porter/Kresge button index
+    { question: "Where is this tree?", correctAnswer: 5, img: 'tree1.jpg' }, // Mchenry button index
+    { question: "Where is this tree?", correctAnswer: 6, img: 'tree1.jpg' }, // Cowell/Stevenson button index
+    { question: "Where is this tree?", correctAnswer: 3, img: 'tree1.jpg' }, // College 9/10 button index
+    { question: "Where is this tree?", correctAnswer: 4, img: 'tree1.jpg' }, // Crown/Merrill button index
+    { question: "Where is this tree?", correctAnswer: 2, img: 'tree1.jpg' } // BSOE button index
 ];
 
 let options = ["Oakes/RCC", "Porter/Kresge", "BSOE", "College 9/10", "Crown/Merrill", "Mchenry", "Cowell/Stevenson"];
